859c533addd8fa7be79b4bf2766b3c530200a9a4
Alex <alexvnep@gmail.com>
2024-04-23 10:09:26 +0200

    Add initial querySelectors and myProject button and import todoContainer.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+import { getTodoContainer } from "./todoContainer";
+
+const container = document.querySelector(".container");
+const navBar = document.querySelector(".nav");
+const addTaskBtn = document.querySelector(".addTaskBtn");
+const searchBtn = document.querySelector(".searchBtn");
+const inboxBtn = document.querySelector(".inboxBtn");
+const todayBtn = document.querySelector(".todayBtn");
+
+const myProjects = document.createElement("button");
+myProjects.classList.add("myProjectTab");
+myProjects.textContent = "My Projects";
+navBar.appendChild(myProjects);
+
+container.append(getTodoContainer());
 console.log("I worked");
