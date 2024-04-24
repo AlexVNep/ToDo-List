9cbe4d423dff27bbed201504b35f619c204f5779
Alex <alexvnep@gmail.com>
2024-04-24 08:17:21 +0200

    Refactor code to make html using only DOM

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
-import { getTodoContainer } from "./todoContainer";
+import { createTodoElements } from "./todoContainer";
+import "./style.css";
 
-const container = document.querySelector(".container");
-const navBar = document.querySelector(".nav");
-const addTaskBtn = document.querySelector(".addTaskBtn");
-const searchBtn = document.querySelector(".searchBtn");
-const inboxBtn = document.querySelector(".inboxBtn");
-const todayBtn = document.querySelector(".todayBtn");
-
-const myProjects = document.createElement("button");
-myProjects.classList.add("myProjectTab");
-myProjects.textContent = "My Projects";
-navBar.appendChild(myProjects);
-
-container.append(getTodoContainer());
+createTodoElements();
 console.log("I worked");
