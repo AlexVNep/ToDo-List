e579c7565313337ae146f81f421f0f63f3d8c899
Alex <alexvnep@gmail.com>
2024-04-25 10:29:20 +0200

    Import projects.js and remove navBar()

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { createTodoElements } from "./todoContainer";
 import { navBar } from "./navBar";
 import "./style.css";
+import { projects } from "./projects";
 
-navBar();
+projects();
 createTodoElements();
 console.log("I worked");
