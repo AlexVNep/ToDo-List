c26075b8221b77687317b4b5e7e376d831b8bd08
Alex <alexvnep@gmail.com>
2024-04-25 15:26:58 +0200

    Add initial inbox page

---
+++ src/inboxPage.js
@@ -0,0 +1,9 @@
+const inboxDiv = document.createElement("div");
+inboxDiv.classList.add("inboxDiv");
+
+const inboxHeading = document.createElement("h1");
+inboxHeading.textContent = "Inbox";
+inboxDiv.appendChild(inboxHeading);
+export function getInboxPage() {
+  return inboxDiv;
+}
