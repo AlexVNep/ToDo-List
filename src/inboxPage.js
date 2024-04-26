ab00c5b22f4340ca52bcfcf9e1842f57035500ed
Alex <alexvnep@gmail.com>
2024-04-26 15:42:38 +0200

    Add class to inboxHeading

--- src/inboxPage.js
+++ src/inboxPage.js
@@ -2,6 +2,7 @@ const inboxDiv = document.createElement("div");
 inboxDiv.classList.add("inboxDiv");
 
 const inboxHeading = document.createElement("h1");
+inboxHeading.classList.add("inboxHeading");
 inboxHeading.textContent = "Inbox";
 inboxDiv.appendChild(inboxHeading);
 export function getInboxPage() {
