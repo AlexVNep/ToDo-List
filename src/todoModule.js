3f6e74cedea6631ce49ca0f94de3004725d5d86d
Alex <alexvnep@gmail.com>
2024-05-08 09:41:54 +0200

    Add tag to class constructor

--- src/todoModule.js
+++ src/todoModule.js
@@ -1,8 +1,9 @@
 export class Todo {
-  constructor(completed, title, dueDate, priority) {
+  constructor(completed, title, dueDate, priority, tag) {
     this.completed = completed;
     this.title = title;
     this.dueDate = dueDate;
     this.priority = priority;
+    this.tag = tag;
   }
 }
