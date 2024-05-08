b9fd6ca3e50c0d851799a0daec8eb02e3502e745
Alex <alexvnep@gmail.com>
2024-05-08 09:36:08 +0200

    Comment out code related to tags, add comments explaining functions, refactor updateProjectChoiceInput

--- src/projects.js
+++ src/projects.js
@@ -1,12 +1,17 @@
+import { getAddTask } from "./todoContainer";
+import { projectChoiceInput } from "./todoContainer";
+
+//Create project page div
 const projectsPage = document.createElement("div");
 projectsPage.classList.add("projectsPage");
 
+//Create heading for project page
 const projectsTitle = document.createElement("h1");
 projectsTitle.classList.add("projectsTitle");
 projectsTitle.textContent = "Projects";
 projectsPage.appendChild(projectsTitle);
 
-//create projects button element
+//create add project button element
 const addProjectButton = document.createElement("button");
 addProjectButton.classList.add("addProjectButton");
 addProjectButton.textContent = "Add Project";
@@ -14,6 +19,7 @@ addProjectButton.addEventListener("click", addProjectButtonClickHandler);
 
 let form;
 
+//Create form element for add project button
 function createProject() {
   form = document.createElement("form");
   form.addEventListener("submit", submitFormHandler);
@@ -25,6 +31,8 @@ function createProject() {
 
   form.appendChild(projectNameLabel);
   form.appendChild(projectNameInput);
+  // form.appendChild(tagSelectLabel);
+  // form.appendChild(tagSelect);
 
   form.appendChild(submitButton);
   form.appendChild(cancelButton);
@@ -32,38 +40,52 @@ function createProject() {
   return form;
 }
 
+//Add project button click handler to make form appear when add project button clicked
 function addProjectButtonClickHandler() {
   const newProject = createProject();
   projectsPage.appendChild(newProject);
 }
 
+//Submit form handler to add project
 function submitFormHandler(event) {
   event.preventDefault();
 
+  //projectTitle is = to project name input
   const projectTitle = document.querySelector("#projectNameInput").value;
+  // const projectTag = document.querySelector("#tagInput").value;
 
+  //On submit, create container for each project
   const projectsContainer = document.createElement("div");
   projectsContainer.classList.add("projectsContainer");
 
+  //on submit, creates a div within the project container
   const projectBlock = document.createElement("div");
   projectBlock.classList.add("projectBlock");
 
+  //Creates a delete button for each project
   const completedButton = document.createElement("button");
   completedButton.classList.add("todoCompleted");
   completedButton.textContent = "Delete";
   completedButton.addEventListener("click", removeTaskHandler);
 
+  //Creates a heading for each project which is = to the project title
   const projectName = document.createElement("h2");
   projectName.classList.add("projectName");
   projectName.textContent = projectTitle;
 
+  // const projectTagSpan = document.createElement("span"); //span should =
+  // projectTagSpan.classList.add("projectTag");
+  // projectTagSpan.textContent = projectTag;
+
   projectsContainer.appendChild(projectBlock);
   projectBlock.appendChild(completedButton);
   projectBlock.appendChild(projectName);
+  // projectBlock.appendChild(projectTagSpan);
 
   projectsPage.appendChild(projectsContainer);
 
   document.querySelector("#projectNameInput").value = "";
+  // document.querySelector("#tagInput").value = "";
 
   projectsPage.removeChild(form);
 }
@@ -79,12 +101,31 @@ function removeTaskHandler() {
 const projectNameLabel = document.createElement("label");
 projectNameLabel.textContent = "Title:";
 const projectNameInput = document.createElement("input");
+// projectNameInput.addEventListener("input", updateTagInput);
 projectNameInput.setAttribute("type", "text");
 projectNameInput.setAttribute("id", "projectNameInput");
+
+export function updateProjectChoiceInput() {
+  const projectTitle = projectNameInput.value.trim();
+  const projectChoice = projectChoiceInput;
+
+  if (projectTitle !== "") {
+    const option = document.createElement("option");
+    option.textContent = projectTitle;
+    option.value = projectTitle;
+    projectChoice.appendChild(option);
+  }
+}
+
+// const tagSelectLabel = document.createElement("label");
+// tagSelectLabel.textContent = "Tag:";
+// const tagSelect = document.createElement("select");
+// tagSelect.setAttribute("id", "tagInput");
 
 const submitButton = document.createElement("button");
 submitButton.setAttribute("type", "submit");
 submitButton.textContent = "Submit";
+submitButton.addEventListener("click", updateProjectChoiceInput);
 
 projectsPage.appendChild(addProjectButton);
 export function getProjectsPage() {
